8ef04dfdeecd0b24c4e08bc89e9506f5fb926ed5
Alan Coleman <alanc@umich.edu>
2025-08-24 12:32:16 -0400

    add plane unit test

---
+++ python/py_test/test_plane.py
@@ -0,0 +1,18 @@
+import unittest
+
+from c8_tracer_py import Vec3
+from c8_tracer_py import Plane
+
+
+class TestPlane(unittest.TestCase):
+    def test_constructor(self):
+        center = Vec3(0, 0, 0)
+        normal = Vec3(0, 0, 1)
+        plane = Plane(center, normal)
+
+        self.assertEqual(normal, plane.getNormal())
+        self.assertEqual(center, plane.getCenter())
+
+
+if __name__ == "__main__":
+    unittest.main()
