be7891eb5966fbeceebf9f5fb785e9b337cca833
Alan Coleman <alanc@umich.edu>
2025-08-24 13:06:56 -0400

    more tests

--- python/py_test/test_environment.py
+++ python/py_test/test_environment.py
@@ -45,10 +45,19 @@ class TestEnvironment(unittest.TestCase):
         dn_dr = 10.0
         env = environment.LinearRadialEnvironment(center, n0, dn_dr)
 
-        # self.assertEqual(env.get_n(center), n0)
-        self.assertNotEqual(env.get_n(Vec3(0.0, 0.0, 2.0)), n0)
+        self.assertEqual(env.get_n(center), n0)
         self.assertNotEqual(env.get_n(Vec3(1000, 1000, 1000)), n0)
-        self.assertAlmostEqual(env.get_n(Vec3(0.0, 0.0, 2.0)), n0 + dn_dr)
+        self.assertAlmostEqual(env.get_n(center + Vec3(0, 0, 1)), n0 + dn_dr)
+
+    def test_cartesian_linear(self):
+        grad_vec = Vec3(5.5, 0, 0)
+        ref_point = Vec3(0, 0, 0)
+        n0 = 1.5
+        env = environment.CartesianLinearEnvironment(grad_vec, ref_point, n0)
+
+        self.assertEqual(env.get_n(ref_point), n0)
+        one_away = ref_point + grad_vec.normalized()
+        self.assertAlmostEqual(env.get_n(one_away), n0 + grad_vec.norm())
 
 
 if __name__ == "__main__":
